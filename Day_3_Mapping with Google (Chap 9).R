70ef16b2faa814812f6107865b6cd03178d017e9
Nicole Okkers1991 <3409603@myuwc.ac.za>
2019-01-31 19:55:23 +0200

    Updated Answers

--- Day_3_Mapping with Google (Chap 9).R
+++ Day_3_Mapping with Google (Chap 9).R
@@ -3,4 +3,16 @@
 # Nicole Okkers
 # 31 January 2019
 
+# Loading Libraries
+library(tidyverse)
+library(ggmap)
+
+# Load Data
+load("data/cape_point_sites.RData")
+
+cape_point <- get_map(location = c(lon = 18.36519, lat = -34.2352581),
+                      zoom = 10, maptype = 'satellite')
+# load("data/cape_point.RData")
+
+
 
--- Exercise_Day3_Converting from CSV to RDATA.R
+++ Exercise_Day3_Converting from CSV to RDATA.R
@@ -8,3 +8,14 @@ Lam <- read_csv("data/laminaria.csv") # Read the CSV file for laminaria
 save(Lam,file = "data/laminaria.RData") # Converting from CSV to RData
 
 
+# Day 4
+# Exercise 2 : Write a five line paragraph giving a description of library (ggsn) and (scales)
+# Nicole Okkers
+# 31 January 2019
+
+??ggsn
+# Answer: The library(ggsn) is a package in R that is used to create North symbols and scale bars for maps. These symbols are created using "ggplot" and "ggmap" commands.
+      # : The package adds north symbols (there are 18 North Symbol Options), and scale bars (units = Km) to maps, using metric or geographic coordinates.
+      # : This is done using the commands "ggplot" and "ggmap" 
+
+             
